580d51b361886e9fa9523b6a4deca7964a2addc1
OfrenDialsa <ofrendialsa.dev@gmail.com>
2026-01-03 12:59:45 +0700

    feat: Cors Middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
 
+        $middleware->api(append: [
+            \App\Http\Middleware\CorsMiddleware::class,
+        ]);
         $middleware->alias([
             'verified' => EnsureEmailIsVerified::class,
             'admin' => RoleMiddleware::class,
